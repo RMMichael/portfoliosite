670f733d92e0d7d0e22bec25211e94092d030873
Rick Michael <rickmikegull@icloud.com>
2021-03-25 18:50:00 -0700

    added files

---
+++ server/backend/urls.py
@@ -0,0 +1,31 @@
+"""backend URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path, re_path
+from django.shortcuts import render
+
+# index catchall view
+def catchall(request):
+    # filename = os.path.join(settings.WHITENOISE_ROOT, 'index.html')
+    return render(request, 'index.html')
+    # return HttpResponse("Hello, world. test")
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('gitsub/api/', include('gitsub.urls')),
+    re_path(r'.*', catchall)
+]
---
+++ server/gitsub/views.py
@@ -0,0 +1,52 @@
+from django.http import HttpResponse
+import os
+from django.shortcuts import render
+import logging
+from django.http import JsonResponse
+from django.http import FileResponse
+
+logger = logging.getLogger(__name__)
+
+
+def projectfile(request, filedir_name):
+    if os.path.isfile("./Projects/" + filedir_name):
+        print(filedir_name)
+        try:
+            file = open("./Projects/" + filedir_name, 'r')
+            text = file.readlines(15000)
+        except Exception as e:
+            file.close()
+            return JsonResponse({'fileContent': "This binary file is not represented", 'fileExt' : ".txt", 'filePath' : filedir_name})
+
+        filename, fileExt = os.path.splitext("./Projects/" + filedir_name)
+
+        if filename == 'Makefile':
+            fileExt = filename
+        fileExt = fileExt.lower()
+        file.close()
+        return JsonResponse({'fileContent': text, 'fileExt' : fileExt, 'filePath' : filedir_name})
+
+    else:
+        logger.log(level=1, msg=str(filedir_name))
+        path = "./Projects/" + filedir_name  # insert the path to your directory
+        try:
+            files = os.listdir(path)
+        except Exception as e:
+            return JsonResponse({'fileContent': "This binary file is not represented", 'fileExt' : ".txt", 'filePath' : filedir_name})
+
+        types = []
+
+        fLen = len(files)
+        for i in range(fLen - 1, -1 , -1):
+            if files[i] == ".DS_Store" or files[i] == ".idea":
+                files.pop(i)
+                continue
+            fpath = path + "/" + files[i]
+            if os.path.isfile(fpath):
+                types.append("file")
+            else:
+                types.append("dir")
+
+        return JsonResponse({'images': files, 'filePath' : filedir_name, 'types' : types})
+
+
